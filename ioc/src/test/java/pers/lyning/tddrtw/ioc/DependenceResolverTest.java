ea38e233f94fdf8a8a939c0aa286440fb754a997
lyning <645464704@qq.com>
2020-01-11 19:56:02 +0800

    rename test case

--- ioc/src/test/java/pers/lyning/tddrtw/ioc/DependenceResolverTest.java
+++ ioc/src/test/java/pers/lyning/tddrtw/ioc/DependenceResolverTest.java
@@ -17,55 +17,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class DependenceResolverTest {
 
     @Test
-    void resolve_no_constructor_parameter_by_layer() throws Exception {
-        // given
-        DependenceResolver dependenceResolver = new DependenceResolver(NoDependence.class);
-        // when
-        List<Dependence> actualLayerDependencies = dependenceResolver.resolve();
-        // then
-        List<Dependence> expectedLayerDependencies = new ArrayList<>();
-        expectedLayerDependencies.add(new Dependence(1, NoDependence.class));
-        assertThat(actualLayerDependencies)
-                .usingRecursiveFieldByFieldElementComparator()
-                .containsExactlyInAnyOrderElementsOf(expectedLayerDependencies);
-    }
-
-    @Test
-    void resolve_one_constructor_parameter_by_layer() throws Exception {
-        // given
-        DependenceResolver dependenceResolver = new DependenceResolver(OneDependence.class);
-        // when
-        List<Dependence> actualLayerDependencies = dependenceResolver.resolve();
-        // then
-        List<Dependence> expectedLayerDependencies = new ArrayList<>();
-        expectedLayerDependencies.add(new Dependence(1, OneDependence.class));
-        expectedLayerDependencies.add(new Dependence(2, OneDependence.A.class));
-        assertThat(actualLayerDependencies)
-                .usingRecursiveFieldByFieldElementComparator()
-                .containsExactlyInAnyOrderElementsOf(expectedLayerDependencies);
-    }
-
-    @Test
-    void resolve_two_constructor_parameter_by_layer() throws Exception {
-        // given
-        DependenceResolver dependenceResolver = new DependenceResolver(TwoDependence.class);
-        // when
-        List<Dependence> actualLayerDependencies = dependenceResolver.resolve();
-        // then
-        List<Dependence> expectedLayerDependencies = new ArrayList<>();
-        expectedLayerDependencies.add(new Dependence(1, TwoDependence.class));
-        expectedLayerDependencies.add(new Dependence(2, TwoDependence.A.class));
-        expectedLayerDependencies.add(new Dependence(2, TwoDependence.B.class));
-        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerA1.class));
-        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerA2.class));
-        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerB1.class));
-        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerB2.class));
-        assertThat(actualLayerDependencies)
-                .usingRecursiveFieldByFieldElementComparator()
-                .containsExactlyInAnyOrderElementsOf(expectedLayerDependencies);
-    }
-
-    @Test
     void should_failure_when_cyclic_dependency1() throws Exception {
         // given
         DependenceResolver dependenceResolver = new DependenceResolver(CyclicDependency.class);
@@ -83,5 +34,54 @@ class DependenceResolverTest {
         Assert<?, ? extends Throwable> assertThatThrownBy = assertThatThrownBy(dependenceResolver::resolve);
         // then
         assertThatThrownBy.isInstanceOf(CircularReferenceException.class);
+    }
+
+    @Test
+    void should_return_dependencies_when_resolve_no_constructor_parameter() throws Exception {
+        // given
+        DependenceResolver dependenceResolver = new DependenceResolver(NoDependence.class);
+        // when
+        List<Dependence> actualLayerDependencies = dependenceResolver.resolve();
+        // then
+        List<Dependence> expectedLayerDependencies = new ArrayList<>();
+        expectedLayerDependencies.add(new Dependence(1, NoDependence.class));
+        assertThat(actualLayerDependencies)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrderElementsOf(expectedLayerDependencies);
+    }
+
+    @Test
+    void should_return_dependencies_when_resolve_one_constructor_parameter() throws Exception {
+        // given
+        DependenceResolver dependenceResolver = new DependenceResolver(OneDependence.class);
+        // when
+        List<Dependence> actualLayerDependencies = dependenceResolver.resolve();
+        // then
+        List<Dependence> expectedLayerDependencies = new ArrayList<>();
+        expectedLayerDependencies.add(new Dependence(1, OneDependence.class));
+        expectedLayerDependencies.add(new Dependence(2, OneDependence.A.class));
+        assertThat(actualLayerDependencies)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrderElementsOf(expectedLayerDependencies);
+    }
+
+    @Test
+    void should_return_dependencies_when_resolve_two_constructor_parameter() throws Exception {
+        // given
+        DependenceResolver dependenceResolver = new DependenceResolver(TwoDependence.class);
+        // when
+        List<Dependence> actualLayerDependencies = dependenceResolver.resolve();
+        // then
+        List<Dependence> expectedLayerDependencies = new ArrayList<>();
+        expectedLayerDependencies.add(new Dependence(1, TwoDependence.class));
+        expectedLayerDependencies.add(new Dependence(2, TwoDependence.A.class));
+        expectedLayerDependencies.add(new Dependence(2, TwoDependence.B.class));
+        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerA1.class));
+        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerA2.class));
+        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerB1.class));
+        expectedLayerDependencies.add(new Dependence(3, TwoDependence.InnerB2.class));
+        assertThat(actualLayerDependencies)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrderElementsOf(expectedLayerDependencies);
     }
 }
