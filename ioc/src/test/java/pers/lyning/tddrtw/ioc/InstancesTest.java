4ec65d008f73a40e078efc8f107820e4efffa34f
lyning <645464704@qq.com>
2020-01-11 21:06:48 +0800

    补充 test case，覆盖更多场景

--- ioc/src/test/java/pers/lyning/tddrtw/ioc/InstancesTest.java
+++ ioc/src/test/java/pers/lyning/tddrtw/ioc/InstancesTest.java
@@ -4,13 +4,15 @@ import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import pers.lyning.tddrtw.ioc.exception.InstanceNotFountException;
 import pers.lyning.tddrtw.ioc.sample.NoDependence;
+import pers.lyning.tddrtw.ioc.sample.OneDependence;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InstancesTest {
 
     @Test
-    void should_failure_when_get_a_exist_instance() throws Exception {
+    void should_failure_when_get_a_noe_exist_instance() throws Exception {
         // given
         Instances instances = new Instances();
         instances.put(new Instance(new NoDependence()));
@@ -18,5 +20,33 @@ class InstancesTest {
         Assert<?, ? extends Throwable> assertThatThrownBy = assertThatThrownBy(() -> instances.get(Object.class));
         // then
         assertThatThrownBy.isInstanceOf(InstanceNotFountException.class);
+    }
+
+    @Test
+    void should_return_custom_instance_when_instance_existed() throws Exception {
+        // given
+        Instances instances = new Instances();
+        Instance givenInstance = new Instance(new OneDependence(new OneDependence.A()));
+        instances.put(givenInstance);
+        // when
+        Instance actualInstance = instances.get(OneDependence.class);
+        // then
+        assertThat(givenInstance)
+                .usingDefaultComparator()
+                .isEqualToComparingFieldByField(actualInstance);
+    }
+
+    @Test
+    void should_return_string_instance_when_instance_existed() throws Exception {
+        // given
+        Instances instances = new Instances();
+        Instance givenInstance = new Instance("hello");
+        instances.put(givenInstance);
+        // when
+        Instance actualInstance = instances.get(String.class);
+        // then
+        assertThat(givenInstance)
+                .usingDefaultComparator()
+                .isEqualToComparingFieldByField(actualInstance);
     }
 }
