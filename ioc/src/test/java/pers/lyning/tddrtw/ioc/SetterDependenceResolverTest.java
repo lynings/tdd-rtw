fa726c9254a6007ebe60c6b7c888fc8a97d5caae
lyning <645464704@qq.com>
2020-01-13 22:32:50 +0800

    增加循环依赖测试

--- ioc/src/test/java/pers/lyning/tddrtw/ioc/SetterDependenceResolverTest.java
+++ ioc/src/test/java/pers/lyning/tddrtw/ioc/SetterDependenceResolverTest.java
@@ -1,5 +1,6 @@
 package pers.lyning.tddrtw.ioc;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pers.lyning.tddrtw.ioc.sample.type2.SetterDependence;
 
@@ -9,11 +10,44 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SetterDependenceResolverTest {
+    @BeforeEach
+    void setUp() {
+        Registry.clear();
+    }
 
     @Test
-    void should_return_dependencies_when_resolve_one_dependence() throws Exception {
+    void should_return_dependencies_when_resolve_cyclic_dependency() throws Exception {
         // given
-        Registry.add(SetterDependence.class, new TypeSetterProperty<>(SetterDependence.Dependence.class));
+        Registry.add(SetterDependence.Dependence.class);
+        Registry.add(SetterDependence.class,
+                new TypeSetterProperty<>(SetterDependence.Dependence.class),
+                new TypeSetterProperty<>(SetterDependence.CyclicDependence.class),
+                new ValueSetterProperty<>("name", "小明"),
+                new ValueSetterProperty<>("age", 10)
+        );
+        Registry.add(SetterDependence.CyclicDependence.class, new TypeSetterProperty<>(SetterDependence.class));
+        Type type = Registry.get(SetterDependence.class);
+        // when
+        List<Dependence> actualDependencies = new SetterDependenceResolver(type).resolve();
+        // then
+        List<Dependence> expectedDependencies = Arrays.asList(
+                new Dependence(1, type),
+                new Dependence(2, Type.of(SetterDependence.Dependence.class)),
+                new Dependence(2, Type.of(SetterDependence.CyclicDependence.class, new TypeSetterProperty<>(SetterDependence.class)))
+        );
+        assertThat(actualDependencies)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrderElementsOf(expectedDependencies);
+    }
+
+    @Test
+    void should_return_dependencies_when_resolve_multiple_dependence() throws Exception {
+        // given
+        Registry.add(SetterDependence.class,
+                new TypeSetterProperty<>(SetterDependence.Dependence.class),
+                new ValueSetterProperty<>("name", "小明"),
+                new ValueSetterProperty<>("age", 10)
+        );
         Registry.add(SetterDependence.Dependence.class);
         Type type = Registry.get(SetterDependence.class);
         // when
